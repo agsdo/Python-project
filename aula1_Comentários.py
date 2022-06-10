bed02d8e00b0994882af5644c48c97427f33791b
agsdo <agsdeoliveira16@gmail.com>
2022-06-09 21:57:26 -0300

    Sobre Comentários

---
+++ aula1_Comentários.py
@@ -0,0 +1,14 @@
+# Utiliza-se o jogo da velha/cerquilha para inserir comentários, sem alterar o programa. 
+print ('Linha 1')
+print ('Linha 2')
+print ('Linha 3')
+# Para falar sobre qualquer coisa. 
+# Em quantas linhas forem necessárias. 
+print ('Linha 4')
+print ('Linha 5')# ou ao lado da linha do programa.
+print ('Linha 6')  # Algumas plataformas exigem espaço entre o final da linha de comando e o comentário.
+# Não inserir comentário óbvios. Caso não queira que determinada linha do código não seja executada, basta colocar a cerquilha no começo, transformando a linha em um comentário.
+
+
+# Resumindo: Códigos entre """ e """ (no PyCharm) são usados para documentação e, geralmente, não são executados, por este motivo, podem ser usados como "comentários". 
+# A cerquilha (#) é o caractere padrão para criar um comentários em Python. O interpretador não irá executar nenhum comando após uma cerquilha.
